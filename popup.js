05707e02bad5ca517d837c49c82bd7e246a4b9ab
bronisMateusz <mateusz.bronis99@gmail.com>
2021-04-09 10:03:31 +0200

    Change cord writing

--- popup.js
+++ popup.js
@@ -1,5 +1,7 @@
+let cordsArray = [];
 function logMouse(e){
-    console.log("X: ", e.clientX + " Y: " + e.clientY);
+    let cords = { x:e.clientX, y:e.clientY };
+    cordsArray.push(cords);
     let pos = e;
     let dot = document.createElement('div');
     dot.className = "dot";
@@ -8,14 +10,19 @@ function logMouse(e){
     dot.style.left = pos.x - 3 + "px";
     dot.style.top = pos.y -3 + "px";
     document.body.appendChild(dot);
+
+    let cordsString = JSON.stringify(cordsArray);
+    console.log(cordsString);
 }
 
-let start = document.getElementById('startRecording');
-start.addEventListener('click', function () {
-    document.querySelector('#mouse-position').addEventListener('mousemove', logMouse);
-});
+document.addEventListener('DOMContentLoaded', function (){
+    let start = document.getElementById('startRecording');
+    start.addEventListener('click', function () {
+        document.querySelector('#mouse-position').addEventListener('mousemove', logMouse);
+    });
 
-let stop = document.getElementById('stopRecording');
-stop.addEventListener('click', function () {
-    document.querySelector('#mouse-position').removeEventListener('mousemove', logMouse);
-});
+    let stop = document.getElementById('stopRecording');
+    stop.addEventListener('click', function () {
+        document.querySelector('#mouse-position').removeEventListener('mousemove', logMouse);
+    });
+})

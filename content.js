d7387bb9f9f7bb4f2877a4ea91ae01df56f89abf
Olgierd <o.dziminski@gmail.com>
2021-04-23 10:32:15 +0200

    Add buttons to activate/deactivate logmouse script

--- content.js
+++ content.js
@@ -1,28 +1,36 @@
-let heatMapContainer = document.createElement('div');
-let body = document.querySelector('body');
-let pageHeight = body.clientHeight;
-let pageWidth = body.clientWidth;
+document.addEventListener('DOMContentLoaded', function () {
+    let start = document.getElementById('startRecording');
+    start.addEventListener('click', function () {
+        let heatMapContainer = document.createElement('div');
+        let body = document.querySelector('body');
+        let pageHeight = body.clientHeight;
+        let pageWidth = body.clientWidth;
 
-heatMapContainer.style.cssText = 'position:absolute;' +
-    'width:100%;' +
-    'height:' +pageHeight + 'px;' +
-    'opacity:0.3;' +
-    'z-index:2147483647;' +
-    'background:#000;' +
-    'top:0;' +
-    'display:flex;' +
-    'justify-content:space-evenly;' +
-    'flex-wrap:wrap;';
+        heatMapContainer.style.cssText = 'position:absolute;' +
+            'width:100%;' +
+            'height:' + pageHeight + 'px;' +
+            'opacity:0.3;' +
+            'z-index:2147483647;' +
+            'background:#000;' +
+            'top:0;' +
+            'display:flex;' +
+            'justify-content:space-evenly;' +
+            'flex-wrap:wrap;';
 
-let heatMapChildIndex = 1;
-for(let i=1; i<pageHeight/100; i++){
-    for(let j=1; j<pageWidth/100; j++){
-        let heatMapChild = document.createElement('div');
-        heatMapChild.setAttribute("id", ''+heatMapChildIndex);
-        heatMapChild.style.cssText = 'width:100px; height: 100px; background: red; opacity: 0.3;';
-        heatMapContainer.appendChild(heatMapChild);
-        heatMapChildIndex++;
-    }
-}
+        let heatMapChildIndex = 1;
+        for (let i = 1; i < pageHeight / 100; i++) {
+            for (let j = 1; j < pageWidth / 100; j++) {
+                let heatMapChild = document.createElement('div');
+                heatMapChild.setAttribute("id", '' + heatMapChildIndex);
+                heatMapChild.style.cssText = 'width:100px; height: 100px; background: red; opacity: 0.3;';
+                heatMapContainer.appendChild(heatMapChild);
+                heatMapChildIndex++;
+            }
+        }
 
-document.body.appendChild(heatMapContainer);
+        document.body.appendChild(heatMapContainer);
+
+    });
+
+
+})

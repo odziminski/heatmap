e0bdba03cf7225944f42f1131fcd5164ce99244c
bronisMateusz <mateusz.bronis99@gmail.com>
2021-05-12 20:01:03 +0200

    Add more colors and set fixed property to temporary stop button

--- content.js
+++ content.js
@@ -5,7 +5,8 @@ let pageHeight = document.body.clientHeight;
 let pageWidth = document.body.clientWidth;
 
 if(document.body.id !== "heatMapPopupBody"){
-    heatMapContainer.style.cssText = 'position:absolute;' +
+    heatMapContainer.style.cssText =
+        'position:absolute;' +
         'width:100%;' +
         'height:' +pageHeight + 'px;' +
         'z-index:2147483646;' +
@@ -15,7 +16,8 @@ if(document.body.id !== "heatMapPopupBody"){
         'justify-content:space-evenly;' +
         'flex-wrap:wrap;';
 
-    heatMapStop.style.cssText = 'position: absolute;' +
+    heatMapStop.style.cssText =
+        'position: fixed;' +
         'top: 100px;' +
         'right: 50px;' +
         'width: 50px;' +
@@ -61,7 +63,71 @@ if(document.body.id !== "heatMapPopupBody"){
             indexesOfMostEntries.forEach(item => {
                 document.getElementById(''+item).style.cssText =
                     'background: red;' +
-                    'opacity: .2;' +
+                    'opacity: .4;' +
+                    'width: 25px;' +
+                    'height: 25px';
+            });
+            indexesOfMostEntries = [];
+        }
+        for(let i = 20; i < 40; i++){
+            let mostEntries = sortedNumberOfEntries[i];
+            let indexesOfMostEntries = [];
+            let index = -1;
+            while ((index = numberOfEntries.indexOf(mostEntries, index+1)) !== -1){
+                indexesOfMostEntries.push(index);
+            }
+            indexesOfMostEntries.forEach(item => {
+                document.getElementById(''+item).style.cssText =
+                    'background: orange;' +
+                    'opacity: .4;' +
+                    'width: 25px;' +
+                    'height: 25px';
+            });
+            indexesOfMostEntries = [];
+        }
+        for(let i = 40; i < 60; i++){
+            let mostEntries = sortedNumberOfEntries[i];
+            let indexesOfMostEntries = [];
+            let index = -1;
+            while ((index = numberOfEntries.indexOf(mostEntries, index+1)) !== -1){
+                indexesOfMostEntries.push(index);
+            }
+            indexesOfMostEntries.forEach(item => {
+                document.getElementById(''+item).style.cssText =
+                    'background: yellow;' +
+                    'opacity: .4;' +
+                    'width: 25px;' +
+                    'height: 25px';
+            });
+            indexesOfMostEntries = [];
+        }
+        for(let i = 60; i < 80; i++){
+            let mostEntries = sortedNumberOfEntries[i];
+            let indexesOfMostEntries = [];
+            let index = -1;
+            while ((index = numberOfEntries.indexOf(mostEntries, index+1)) !== -1){
+                indexesOfMostEntries.push(index);
+            }
+            indexesOfMostEntries.forEach(item => {
+                document.getElementById(''+item).style.cssText =
+                    'background: green;' +
+                    'opacity: .4;' +
+                    'width: 25px;' +
+                    'height: 25px';
+            });
+            indexesOfMostEntries = [];
+        }
+        for(let i = 80; i < 100; i++){
+            let mostEntries = sortedNumberOfEntries[i];
+            let indexesOfMostEntries = [];
+            let index = -1;
+            while ((index = numberOfEntries.indexOf(mostEntries, index+1)) !== -1){
+                indexesOfMostEntries.push(index);
+            }
+            indexesOfMostEntries.forEach(item => {
+                document.getElementById(''+item).style.cssText =
+                    'background: blue;' +
+                    'opacity: .4;' +
                     'width: 25px;' +
                     'height: 25px';
             });
